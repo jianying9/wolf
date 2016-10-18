531df12103c047710b4f34217afd1340230e89e2
jianying9 <jianying9@192.168.1.122>
2016-10-18 15:14:42 +0800

    单元测试模式启动时输出service信息

--- wolf-framework/src/main/java/com/wolf/framework/worker/build/WorkerBuilder.java
+++ wolf-framework/src/main/java/com/wolf/framework/worker/build/WorkerBuilder.java
@@ -122,7 +122,7 @@ public class WorkerBuilder {
             //创建对应的工作对象
             final ServiceWorker serviceWorker = new ServiceWorkerImpl(workHandler, serviceContext);
             //INFO,开发模式才能会返回接口信息
-            if (compileModel.equals(FrameworkConfig.DEVELOPMENT)) {
+            if (compileModel.equals(FrameworkConfig.DEVELOPMENT) || compileModel.equals(FrameworkConfig.UNIT_TEST)) {
                 serviceWorker.createInfo();
             }
             this.workerBuildContext.putServiceWorker(serviceContext.route(), serviceWorker, clazz.getName());
