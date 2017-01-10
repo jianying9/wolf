6e0f620f819f14f955017f28c9a49756fff3895f
jianying9 <jianying9@192.168.1.122>
2017-01-10 14:46:25 +0800

    1、实体工具类增加获取实体空记录的模板

--- wolf-framework/src/main/java/com/wolf/framework/utils/EntityUtils.java
+++ wolf-framework/src/main/java/com/wolf/framework/utils/EntityUtils.java
@@ -2,6 +2,7 @@ package com.wolf.framework.utils;
 
 import com.wolf.framework.context.ApplicationContext;
 import com.wolf.framework.dao.ColumnHandler;
+import com.wolf.framework.dao.ColumnType;
 import com.wolf.framework.dao.Entity;
 import java.util.Collections;
 import java.util.HashMap;
@@ -13,11 +14,11 @@ import java.util.Map;
  * @author jianying9
  */
 public class EntityUtils {
-    
-    public static <T extends Entity>Map<String, Object> getMap( T t) {
+
+    public static <T extends Entity> Map<String, Object> getMap(T t) {
         Map<String, Object> entityMap = Collections.EMPTY_MAP;
         List<ColumnHandler> columnHandlerList = ApplicationContext.CONTEXT.getEntityInfo(t.getClass());
-        if(columnHandlerList != null) {
+        if (columnHandlerList != null) {
             entityMap = new HashMap<>(columnHandlerList.size(), 1);
             String name;
             Object value;
@@ -29,5 +30,23 @@ public class EntityUtils {
         }
         return entityMap;
     }
-    
+
+    public static <T extends Entity> Map<String, Object> getTempMap(Class<T> clazz) {
+        Map<String, Object> entityMap = Collections.EMPTY_MAP;
+        List<ColumnHandler> columnHandlerList = ApplicationContext.CONTEXT.getEntityInfo(clazz);
+        if (columnHandlerList != null) {
+            entityMap = new HashMap<>(columnHandlerList.size(), 1);
+            String name;
+            Object value;
+            for (ColumnHandler columnHandler : columnHandlerList) {
+                if (columnHandler.getColumnType().equals(ColumnType.KEY) == false) {
+                    name = columnHandler.getColumnName();
+                    value = columnHandler.getDefaultValue();
+                    entityMap.put(name, value);
+                }
+            }
+        }
+        return entityMap;
+    }
+
 }
