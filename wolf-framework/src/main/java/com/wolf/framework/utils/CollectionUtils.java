a1ffae2e4a6be36190bf26d94102d4303ff13c39
jianying9 <jianying9@192.168.1.122>
2017-02-28 15:39:25 +0800

    1、增加集合操作工具类

---
+++ wolf-framework/src/main/java/com/wolf/framework/utils/CollectionUtils.java
@@ -0,0 +1,24 @@
+package com.wolf.framework.utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author jianying9
+ */
+public final class CollectionUtils {
+    
+    public static <O extends Object> List<O> removeRepeated(List<O> list) {
+        List<O> newList = new ArrayList<>(list.size());
+        Set<O> set = new HashSet<>(list.size());
+        for (O o : list) {
+            if(set.add(o)) {
+                newList.add(o);
+            }
+        }
+        return newList;
+    }
+}
