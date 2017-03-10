379a6a8e32a07d2a8c059b210ce782b61b73d263
jianying9 <jianying9@192.168.1.122>
2017-03-10 20:00:29 +0800

    1、删除readis相关测试代码

--- wolf-framework/src/test/java/com/wolf/FrameworkJUnitTest.java
+++ wolf-framework/src/test/java/com/wolf/FrameworkJUnitTest.java
@@ -49,10 +49,6 @@ public class FrameworkJUnitTest {
         parameterMap.put(FrameworkConfig.TASK_CORE_POOL_SIZE, "10");
         parameterMap.put(FrameworkConfig.TASK_MAX_POOL_SIZE, "20");
         //
-        parameterMap.put(FrameworkConfig.REDIS_SERVER_HOST, "192.168.59.49");
-        parameterMap.put(FrameworkConfig.REDIS_SERVER_PORT, "6379");
-        parameterMap.put(FrameworkConfig.REDIS_MAX_POOL_SIZE, "20");
-        parameterMap.put(FrameworkConfig.REDIS_MIN_POOL_SIZE, "10");
         ApplicationContextBuilder applicationContextBuilder = new ApplicationContextBuilder(parameterMap);
         applicationContextBuilder.build();
     }
